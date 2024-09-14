93799df9fabade72bda8eae6e129294db9acc137
Kinleoapple2460 <kinleoapple2460@gmail.com>
2024-09-14 14:02:23 +1000

    Updated Something.

---
+++ bin/src/handler/get_blog_handler.rs
@@ -0,0 +1,5 @@
+use warp::{Rejection, Reply};
+
+pub fn get_blog_handler() -> Result<Box<dyn Reply>, Rejection> {
+    todo!()
+}
--- bin/src/handler/mod.rs
+++ bin/src/handler/mod.rs
@@ -1,3 +1,4 @@
 pub mod rsa_handler;
 pub mod login_handler;
 pub mod token_login_handler;
+pub mod get_blog_handler;
